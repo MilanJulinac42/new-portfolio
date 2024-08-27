2f012525a4ab7ac35ab857a381b41156215ce3cc
MilanJulinac42 <milanjulinac996@gmail.com>
2024-08-27 22:40:22 +0200

    added theme

--- blog-test/astro.config.mjs
+++ blog-test/astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
 export default defineConfig({
-    // staviti url
-  site: "https://example.com"
+  // staviti url
+  site: "https://example.com",
+  integrations: [preact()]
 });
